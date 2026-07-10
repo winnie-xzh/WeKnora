765fc2e2704fb6d21a48dd04b5054c99dcb5c5db
winnie <winnie.xzh@gmail.com>
2026-07-10 12:44:22 +0800

    test: deploy timing 2

--- internal/handler/system.go
+++ internal/handler/system.go
@@ -159,7 +159,7 @@ type GetSystemInfoResponse struct {
 // 编译时注入的版本信息
 var (
 	Version   = "test"
-	Edition   = "standard-v3"
+	Edition   = "standard-v4"
 	CommitID  = "unknown"
 	BuildTime = "unknown"
 	GoVersion = "unknown"
