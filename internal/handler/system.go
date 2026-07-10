0dcb84bafd72188a0cfa89abcb369646e9b5f30e
winnie <winnie.xzh@gmail.com>
2026-07-10 14:25:48 +0800

    test: bump Edition to standard-v10

--- internal/handler/system.go
+++ internal/handler/system.go
@@ -159,7 +159,7 @@ type GetSystemInfoResponse struct {
 // 编译时注入的版本信息
 var (
 	Version   = "test"
-	Edition   = "standard-v9"
+	Edition   = "standard-v10"
 	CommitID  = "unknown"
 	BuildTime = "unknown"
 	GoVersion = "unknown"
